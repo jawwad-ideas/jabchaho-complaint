2254f3c64a589b7e2ca9bf175ca993c6c3e2fec1
saroopchand <saroop.chand@ideas.com.pk>
2024-11-18 16:28:54 +0500

    images upload

--- resources/views/backend/orders/index.blade.php
+++ resources/views/backend/orders/index.blade.php
@@ -14,6 +14,12 @@
                         class="btn btn-sm rounded bg-theme-dark-300 text-light me-2 border-0 fw-bold d-flex align-items-center p-2 gap-2"><i
                             class="fa fa-solid fa-user-plus"></i><span>New Order</span></small>
                 </a>
+
+                <small id="showFilterBox" type="button"
+                       class="btn btn-sm rounded bg-theme-dark-300 text-light me-2 border-0 fw-bold d-flex align-items-center p-2 gap-2"><i
+                        class="fa fa-solid fa-filter"></i> <span>Filter</span>
+                </small>
+
             </div>
         </div>
 
@@ -22,13 +28,38 @@
 
         <div class="bg-light p-2 rounded">
 
-
-            {{-- <form class="form-inline" method="GET">
-            <input type="hidden" id="dashboardFilter" placeholder="dashboardFilter" name="dashboard_filter"
-                maxlength="50" value="{{ $dashboardFilter}}">
-        </form> --}}
+             {{--<form class="form-inline" method="GET">
+                <input type="text" id="order_number" placeholder="order number" name="order_number"
+                    maxlength="50" value="{{ $order_number}}">
+            </form>--}}
             <!--Assign To Modal -->
             <div id="modalDiv"></div>
+
+            <div class="" id="filterBox" style="display:none;">
+                <form class="form-inline" method="GET" action="{{ route('orders.index') }}">
+                    <div class="row mb-3">
+                        <div class="col-lg-8 d-flex flex-wrap">
+                            <div class="col-sm-6 px-2">
+                                <input type="text" class="form-control p-2" autocomplete="off" name="order_number"
+                                       value="{{ $order_number ?? '' }}" placeholder="Order Number">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-12 text-end mt-4">
+                            <button type="submit"
+                                    class="btn bg-theme-yellow text-dark p-2 d-inline-flex align-items-center gap-1"
+                                    id="consult">
+                                <span>Search</span>
+                                <i alt="Search" class="fa fa-search"></i>
+                            </button>
+                            <a href="{{ route('orders.index') }}"
+                               class="btn bg-theme-dark-300 text-light p-2 d-inline-flex align-items-center gap-1 text-decoration-none">
+                                <span>Clear</span>
+                                <i class="fa fa-solid fa-arrows-rotate"></i></a>
+                        </div>
+                    </div>
+                </form>
+            </div>
 
             <div class="d-flex my-2">
                 Showing results {{ ($orders->currentPage() - 1) * config('constants.per_page') + 1 }} to
@@ -76,4 +107,11 @@
     </div>
     <!--Assign To Modal -->
     <div id="modalDiv"></div>
+
+    <script>
+        $("#showFilterBox").click(function() {
+            $("#filterBox").toggle();
+        });
+    </script>
+
 @endsection
