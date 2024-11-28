de84b53fa593323a98aecd436f60aae26f84c2c8
Syed Jawwad Shamim <jawwad.shamim@ideas.com.pk>
2024-11-28 12:27:02 +0500

    bk-color changed.

--- resources/views/backend/emails/orderCompleted.blade.php
+++ resources/views/backend/emails/orderCompleted.blade.php
@@ -7,14 +7,14 @@
                         <tr>
                             <td class="header" style="vertical-align:top;background-color:#ec23262b;padding:40px 25px 25px;">
                                 <div>
-                                    <a class="logo" href="#" style="color:#ed2024;text-decoration:none" target="_other" rel="nofollow">
+                                    <a class="logo" href="#" style="color:#000;text-decoration:none" target="_other" rel="nofollow">
                                         <img width="190" height="39" src="{{ $app_url }}/assets/images/jc-logo.png" alt="Jabchaho" style="border:0;height:auto;line-height:100%;outline:none;text-decoration:none;max-width:150px">
                                     </a>
                                 </div>
                             </td>
                         </tr>
                         <tr>
-                            <td class="main-content" style="vertical-align:top;background-color:#efef;padding:30px;">
+                            <td class="main-content" style="vertical-align:top;background-color:#fff;padding:30px;">
                                 <p class="greeting" style="margin-top:0;margin-bottom:10px">Dear {{$name}},</p>
                                 <p style="margin-top:0;margin:20px 0">The details of your Order No. {{$orderNo}} are provided below.</p>
                                 <p class="greeting" style="margin-top:0;margin-bottom:10px">
