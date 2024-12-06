2198cbc166bb36a7c2044efec34c18a7778d7153
Syed Jawwad Shamim <jawwad.shamim@ideas.com.pk>
2024-12-06 11:51:54 +0500

    add check for    * check if user login and hit domain [https://complaint.jabchaho.com]    * donot have segment1 permission then redirect to first .index route permission

--- app/Http/Middleware/PermissionMiddleware.php
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Spatie\Permission\Exceptions\UnauthorizedException;
+use App\Helpers\Helper;
 
 class PermissionMiddleware
 {
@@ -22,7 +23,7 @@ class PermissionMiddleware
         if ($authGuard->guest()) {
             throw UnauthorizedException::notLoggedIn();
         }
-
+       
         if (! is_null($permission)) {
             $permissions = is_array($permission)
                 ? $permission
@@ -35,10 +36,21 @@ class PermissionMiddleware
             $permissions = array($permission);
         }
         
-
         foreach ($permissions as $permission) {
             if ($authGuard->user()->can($permission)) {
                 return $next($request);
+            }
+            else{
+                /*
+                 * check if user login and hit domain [https://complaint.jabchaho.com] 
+                 * donot have segment1 permission then redirect to first .index route permission
+                 */
+    
+                $segment1 = $request->segment(1);
+                if (app('auth')->check() && app('auth')->user() && empty($segment1))
+                { 
+                    return Helper::authenticated(app('auth')->user());
+                }
             }
         }
 
