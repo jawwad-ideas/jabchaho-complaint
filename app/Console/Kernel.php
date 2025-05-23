4dc11e4e789fab4ce8de4b8ca9999248e88f23a4
Syed Jawwad Shamim <jawwad.shamim@ideas.com.pk>
2025-05-23 10:46:52 +0500

    remove s from cron name.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->command('sync:laundry-orders')->withoutOverlapping()->everyFifteenMinutes();
         $schedule->command('remove:laundry-orders')->withoutOverlapping()->dailyAt('00:00'); // Runs at 12:00 AM;
-        $schedule->command('release:hold-whatsapps')->withoutOverlapping()->dailyAt('10:00'); 
+        $schedule->command('release:hold-whatsapp')->withoutOverlapping()->dailyAt('10:00'); 
     }
 
     /**
